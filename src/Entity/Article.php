b5ba6f428e018b2a1430d27a35f4c160b9bbfa6d
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-22 09:26:55 +0200

    Update entity category

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -42,6 +42,12 @@ class Article
      */
     private $createAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -91,6 +97,18 @@ class Article
     public function setCreateAt(\DateTimeInterface $createAt): self
     {
         $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
